0080b3422488bf6cc544b67585253e60fffa79fc
cao7113 <cao7113@hotmail.com>
2012-02-23 14:03:00 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shareup::Application.config.secret_token = '998527c3bb7c98bd561550523555e4b6535d5de2ca15015a6fc0e83b87216b2a564d472e9309efc488b3af83f78dbfa14814523a2c3b0c2c849de4c91ced55d3'
